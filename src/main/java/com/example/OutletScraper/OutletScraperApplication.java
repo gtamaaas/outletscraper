8477c14d8cc2fbe2e9c868bc884a920f6318c800
gtamaaas <gaspar.tamas315@gmail.com>
2026-01-27 19:33:34 +0200

    initial commit

---
+++ src/main/java/com/example/OutletScraper/OutletScraperApplication.java
@@ -0,0 +1,13 @@
+package com.example.OutletScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutletScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutletScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OutletScraper/OutletScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OutletScraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutletScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
