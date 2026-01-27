7bc0539612fb62eac3416a4cd9653fe40aba9305
gtamaaas <gaspar.tamas315@gmail.com>
2026-01-27 23:03:30 +0200

    added mongoconfig

---
+++ src/main/java/com/example/OutletScraper/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package com.example.OutletScraper.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory() {
+        MongoClient mongoClient = MongoClients.create("mongodb://127.0.0.1:27017");
+        return new SimpleMongoClientDatabaseFactory(mongoClient, "databasenamehere");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory());
+    }
+}
