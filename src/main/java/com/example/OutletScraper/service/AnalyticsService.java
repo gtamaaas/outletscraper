fd72e338cfe7f33bfd0409dbd0c90773e7b0bbc3
gtamaaas <gaspar.tamas315@gmail.com>
2026-01-29 12:59:59 +0200

    AnalyticsService and AnalyticsServiceTest

---
+++ src/main/java/com/example/OutletScraper/service/AnalyticsService.java
@@ -0,0 +1,34 @@
+package com.example.OutletScraper.service;
+
+import com.example.OutletScraper.model.Item.Item;
+import com.example.OutletScraper.model.Item.ScrapeObservation;
+import com.example.OutletScraper.repository.ItemRepository;
+import com.example.OutletScraper.repository.ScrapeObservationRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+
+@Service
+public class AnalyticsService {
+
+    private final ItemRepository itemRepository;
+
+    private final ScrapeObservationRepository scrapeObservationRepository;
+
+    public AnalyticsService(ItemRepository itemRepository, ScrapeObservationRepository scrapeObservationRepository) {
+        this.itemRepository = itemRepository;
+        this.scrapeObservationRepository = scrapeObservationRepository;
+    }
+
+    public double calculateLowestPrice(Item item) {
+        return scrapeObservationRepository.findAllByItemId(item.getId())
+                .stream()
+                .map(ScrapeObservation::getPrice)
+                .min(Double::compareTo)
+                .orElseThrow(() -> new IllegalStateException("No price observations found"));
+    }
+
+
+}
---
+++ src/test/java/com/example/OutletScraper/AnalyticsServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.OutletScraper;
+
+import com.example.OutletScraper.model.Item.Item;
+import com.example.OutletScraper.model.Item.ScrapeObservation;
+import com.example.OutletScraper.repository.ScrapeObservationRepository;
+import com.example.OutletScraper.service.AnalyticsService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Incubating;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AnalyticsServiceTest {
+
+    @MockitoBean
+    private ScrapeObservationRepository scrapeObservationRepository;
+
+    @Autowired
+    private AnalyticsService analyticsService;
+
+
+    @Test
+    void shouldReturnLowestPrice() {
+
+        Item item = new Item();
+        item.setId("test");
+
+        ScrapeObservation obs1 = mock(ScrapeObservation.class);
+        ScrapeObservation obs2 = mock(ScrapeObservation.class);
+        ScrapeObservation obs3 = mock(ScrapeObservation.class);
+
+        when(obs1.getPrice()).thenReturn(500.0);
+        when(obs2.getPrice()).thenReturn(5.0);
+        when(obs3.getPrice()).thenReturn(25.99);
+
+        when(scrapeObservationRepository.findAllByItemId(item.getId()))
+                .thenReturn(List.of(obs1, obs2, obs3));
+
+
+        double result = analyticsService.calculateLowestPrice(item);
+
+
+        assertEquals(5.0, result);
+    }
+}
